0eebd5ee23bb28af1ce342441f4ba77e87a29bf5
Andrew Mayer <andrewmayer515@gmail.com>
2021-02-08 17:44:09 -0600

    progress

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import puppeteer from 'puppeteer';
 
+require('dotenv').config();
+
 export const main = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
