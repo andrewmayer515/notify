ee6a171090026270a4b571e70f7f504e24e713e6
Andrew Mayer <andrewmayer515@gmail.com>
2021-02-08 21:21:45 -0600

    everything works

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# notify
+# Notify
+
+Create .env at root with the following structure:
+```
+EMAIL_USER=example@domain.com
+EMAIL_PASS=password
+PING_DELAY=30
+WEB_URL=https://www.something.com/
+WEB_SELECTOR='.className'
+```
--- index.js
+++ index.js
@@ -1,13 +1,67 @@
+/* eslint-disable no-await-in-loop */
 import puppeteer from 'puppeteer';
+import nodemailer from 'nodemailer';
 
 require('dotenv').config();
 
-export const main = async () => {
+const openBrowser = async () => {
   const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+  return browser.newPage();
+};
 
-  await page.goto('https://example.com');
-  await page.screenshot({ path: 'photo.png' });
+const searchLoop = async page => {
+  let notFound = true;
+  let firstPassthroughText = '';
+
+  while (notFound) {
+    await new Promise(resolve => setTimeout(resolve, process.env.PING_DELAY * 1000));
+    await page.goto(process.env.WEB_URL, { waitUntil: 'networkidle2' });
+    await page.bringToFront();
+
+    const elementText = await page.$eval(process.env.WEB_SELECTOR, el => el.textContent);
+
+    // Determine if something has changed
+    if (firstPassthroughText) {
+      firstPassthroughText = elementText;
+    }
+
+    notFound = elementText === firstPassthroughText;
+  }
+
+  await page.screenshot({ path: 'attachment.png' });
+};
+
+const sendNotification = async () => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: process.env.EMAIL_USER,
+    subject: '-- NOTIFY ALERT -- ',
+    text: 'Something has changed!',
+    attachments: [
+      {
+        path: 'attachment.png',
+      },
+    ],
+  };
+
+  const info = await transporter.sendMail(mailOptions);
+  console.log(`Message sent: ${info.messageId}`);
+};
+
+const main = async () => {
+  const page = await openBrowser();
+  await searchLoop(page);
+  await sendNotification();
+
+  // End
+  process.exit(0);
 };
 
 main();
